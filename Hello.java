15d3e4374a8160942868ef11865896a2ec6a3a11
3sunshine3 <48208726+3sunshine3@users.noreply.github.com>
2019-03-05 10:56:47 +0800

    修改的文件
    修改的文件：使用 github 编辑的第一个文件

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class Hello{
 
      public static void main(String[] args){
      
-     system.out.println("Hello World!!!");
+     system.out.println("Hello! 使用 github 编辑的第一个文件");
      
      }
 
